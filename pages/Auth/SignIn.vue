0385d5f35f53f00f75d7766dfd625ae752c5eb1f
github username <github email address>
2019-05-01 21:11:28 -0500

    Add Auth folder, Add Sign In

---
+++ pages/Auth/SignIn.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="signin-form">
+    <form novalidate="true">
+      <div v-if="errors.length">
+        <ul>
+          <li v-for="(error, idx) in errors" :key="idx">{{ error }}</li>
+        </ul>
+      </div>
+      <input
+        id="email"
+        v-model="email"
+        class="f4 pa2 input-reset ba mb3 w-100 b--moon-gray"
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        id="password"
+        v-model="password"
+        class="f4 pa2 input-reset ba mb3 w-100 b--moon-gray"
+        type="password"
+        placeholder="Password"
+      />
+      <button
+        class="f6 link dim ba bw1 ph3 pv2 mb2 dib dark-green"
+        @click.prevent="handleSubmit"
+      >
+        Sign In
+      </button>
+      <nuxt-link class="f6 link dim ba bw1 ph3 pv2 mb2 dib mid-gray" to="/"
+        >Cancel</nuxt-link
+      >
+      <p>Sign Up</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+      errors: []
+    }
+  },
+  methods: {
+    validateEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+    handleSubmit() {
+      if (!this.email || !this.validateEmail(this.email)) {
+        this.errors.push('Valid email required.')
+      }
+      if (!this.password) {
+        this.errors.push('Password required.')
+      }
+
+      if (!this.errors.lenght) {
+        console.log('call auth/signin')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,13 +3,21 @@ import 'firebase/database'
 
 if (!firebase.apps.length) {
   const config = {
-    apiKey: 'AIzaSyB3VzSfA1N3Nywj4Tmqt-OPW89xCEnzD2Y',
-    authDomain: 'ytgallery-d8934.firebaseapp.com',
-    databaseURL: 'https://ytgallery-d8934.firebaseio.com',
-    projectId: 'ytgallery-d8934',
-    storageBucket: 'ytgallery-d8934.appspot.com',
-    messagingSenderId: '890683006966'
+    apiKey: 'AIzaSyDhIgnq1KnnDyrQY3-H69OPyIxLLtUY3qE',
+    authDomain: 'jeg-expensify.firebaseapp.com',
+    databaseURL: 'https://jeg-expensify.firebaseio.com',
+    projectId: 'jeg-expensify',
+    storageBucket: 'jeg-expensify.appspot.com',
+    messagingSenderId: '218734481166'
   }
+  // const config = {
+  //   apiKey: 'AIzaSyB3VzSfA1N3Nywj4Tmqt-OPW89xCEnzD2Y',
+  //   authDomain: 'ytgallery-d8934.firebaseapp.com',
+  //   databaseURL: 'https://ytgallery-d8934.firebaseio.com',
+  //   projectId: 'ytgallery-d8934',
+  //   storageBucket: 'ytgallery-d8934.appspot.com',
+  //   messagingSenderId: '890683006966'
+  // }
 
   firebase.initializeApp(config)
 }
