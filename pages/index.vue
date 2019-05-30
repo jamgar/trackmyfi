ac0b8bac09fe534bbe78330f8047e436339f5a4a
github username <github email address>
2019-05-29 20:39:05 -0500

    Finished Index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,42 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">Track My Fi</h1>
-      <h2 class="subtitle">Track Income and Expenses</h2>
-      <p class="content">
-        Starting a budget is not alway easy, but you will want to begin by
-        tracking your finances. Track My Fi was built to do just that. By
-        tracking your what comes in and what goes out you will have a baseline
-        to start from.
-      </p>
+  <div>
+    <div class="vh-75 dt w-100">
+      <div class="dtc v-mid dark-grey tc ph3 ph4-l">
+        <h1 class="f-headline lh-solid">Track My Fi</h1>
+        <h2 class="f2">Track Income and Expenses</h2>
+        <p class="f3 lh-copy">
+          Starting a budget is not alway easy, but you will want to begin by
+          tracking your finances. Track My Fi was built to do just that. By
+          tracking your what comes in and what goes out you will have a baseline
+          to start from.
+        </p>
+      </div>
     </div>
-  </section>
+    <div class="tc ph5">
+      <h3 class="f2">Features</h3>
+      <div class="pv3">
+        <p class="f3">Chart for Annual Expense and Deposits</p>
+        <img src="../assets/images/annual_chart.png" alt="annual chart" />
+      </div>
+      <div class="pv3">
+        <p class="f3">Chart of current month Expenses by category</p>
+        <img src="../assets/images/expense_chart.png" alt="annual chart" />
+      </div>
+      <div class="pv3">
+        <p class="f3">Chart of current month Deposits by category</p>
+        <img src="../assets/images/Deposits_chart.png" alt="annual chart" />
+      </div>
+    </div>
+    <footer class="bg-dark-gray white pv5 pv6-l ph4 tc mt6">
+      <p class="f6">
+        <span class="dib mr4 mr5-ns">2019 By James Garcia</span>
+      </p>
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 85vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.content {
-  text-align: center;
-  color: #526488;
-  padding: 0 4rem;
-  font-size: 1.5rem;
-}
-</style>
+<style></style>
